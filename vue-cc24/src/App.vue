5fd4f3b68ea918667f817d4b6bde15ee67c6204e
tosvic <victoruhuo@yahoo.com>
2024-08-19 23:07:27 +0100

    vue router

--- vue-cc24/src/App.vue
+++ vue-cc24/src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
 import Navbar from "./Navbar.vue";
-import Hero from "./Hero.vue";
-import HomeCard from "./HomeCard.vue";
-import JobList from "./JobList.vue";
-import Joblistings from "./Joblistings.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <Navbar />
-  <Hero title="hello peole" subtitle="chilling with props"/>
-  <HomeCard />
-  <!-- <JobList /> -->
-  <Joblistings :limit="3" :showButton="true" />
+  <RouterView />
 </template> 
---
+++ vue-cc24/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Hero from "@/Hero.vue";
+import HomeCard from "@/HomeCard.vue";
+import JobList from "@/JobList.vue";
+import Joblistings from "@/Joblistings.vue";
+</script>
+
+<template>
+  <Hero title="hello peole" subtitle="chilling with props"/>
+  <HomeCard />
+  <!-- <JobList /> -->
+  <Joblistings :limit="3" :showButton="true" />
+</template>
